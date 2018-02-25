ec9656a6da34aa18c9f3fb072e1d96c199fe7272
kaweue <pawel.marchewka1@gmail.com>
2018-02-25 15:26:46 +0100

    Added some integration tests (#5)
    * Added some integration tests
    
    * Updated Gopkg.lock

--- integration_tests/integration.py
+++ integration_tests/integration.py
@@ -12,5 +12,27 @@ class UmWarsawClientIntegration(unittest.TestCase):
         self.assertEqual(bus_stop['BusID'], '5104', "Incorrect bus id")
         self.assertEqual(bus_stop['Name'], 'Znana', "Incorrect bus name")
 
+    def test_get_lines_at_bus_stop(self):
+        api_key = os.environ.get('API_KEY')
+        output = subprocess.check_output(['api-um-warsaw-client', '--api-key', api_key,
+                                          'getLinesAtBusStop', '5104', '01'])
+        lines = json.loads(output)
+        self.assertEqual(len(lines), 3)
+        self.assertSequenceEqual(lines, ['129', '155', '167'])
+
+    def test_get_time_table(self):
+        api_key = os.environ.get('API_KEY')
+        output = subprocess.check_output(['api-um-warsaw-client', '--api-key', api_key,
+                                          'getTimeTable', '5104', '01', '155'])
+        timeTable = json.loads(output)
+        self.assertGreater(len(timeTable), 0)
+        for record in timeTable:
+            self.assertIn("Brigade", record)
+            self.assertIn("Direction", record)
+            self.assertIn("Time", record)
+            self.assertIsNotNone(record['Brigade'])
+            self.assertIsNotNone(record['Direction'])
+            self.assertIsNotNone(record['Time'])
+
 if __name__ == "__main__":
     unittest.main()
